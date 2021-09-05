fb3f467badbf3bc77f22312d4e6160a9a045e777
jueviole-grace <akshatgauriiitsurat@gmail.com>
2021-09-05 20:00:54 +0530

    this is greeting card tempelate
    to create a card using it just add the urls of the images you want in it and in the terminal hit 'npm start'

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import BirthdayCards from './BirthdayCards.js'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <h2>🎂🎉Happy Birthday Mummy🎂🎉</h2>
+      </div>
+      <BirthdayCards />
+    </div>
+  );
+}
+
+export default App;
